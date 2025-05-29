e34c00c73b9358e6f4127b241857bb3eb3bebe43
web-dev-pasta <on5100832@gmail.com>
2025-05-29 08:01:31 +0300

    Added Project Files

---
+++ README.md
@@ -0,0 +1,9 @@
+## Welcome! 👋
+
+# Frontend Mentor - Interactive rating component
+
+![Design preview for the Interactive rating component coding challenge](./preview.jpg)
+
+## Challenge Solved😃.
+
+Find Live Demo Here :
---
+++ script.js
@@ -0,0 +1,18 @@
+let allDivs = document.querySelectorAll(".card .rate div");
+let x;
+let card = document.querySelector(".card");
+let submitCard = document.querySelector(".submit-card");
+allDivs.forEach((e) => {
+  e.onclick = function () {
+    allDivs.forEach((e) => e.classList.remove("selected"));
+    e.classList.add("selected");
+    x = e.innerHTML;
+  };
+});
+let pSelection = document.querySelector("p.selection");
+let submitButton = document.querySelector(".submit");
+submitButton.addEventListener("click", () => {
+  card.style.display = "none";
+  pSelection.innerHTML = `You selected ${x} out of 5`;
+  submitCard.style.display = "block";
+});
